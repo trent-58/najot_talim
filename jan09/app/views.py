5b212bad02ce4cc817632c846fdfb19a6eb6a917
Jace Trent <trentjace58@gmail.com>
2026-01-11 19:08:54 +0500

    jan09 hw done

---
+++ jan09/app/views.py
@@ -0,0 +1,15 @@
+from rest_framework import generics, viewsets
+from .models import Author, Book
+from .serializers import AuthorSerializer, BookSerializer
+
+class AuthorViewSet(viewsets.ModelViewSet):
+    queryset = Author.objects.all()
+    serializer_class = AuthorSerializer
+
+class BookListCreateView(generics.ListCreateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+class BookDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
