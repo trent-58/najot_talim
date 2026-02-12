c188172a64e24f1a01edab9d8c3bcceea4859ec4
Jace Trent <trentjace58@gmail.com>
2026-02-12 16:06:47 +0000

    a

--- feb04/accounts/views.py
+++ feb04/accounts/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.views.generic import CreateView
+
+from jan22.users.serializers import SignUpSerializer
+from .models import CustomUser
 
 # Create your views here.
+class SignUpView(CreateView):
+    serializer_class = SignUpSerializer
+    queryset = CustomUser.objects.all()
